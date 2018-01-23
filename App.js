14d6be0d0c46b5f0bd436846dfb621d93c44e0ac
HieuBui <buitronghieu.ptit91@gmail.com>
2018-01-23 07:00:16 +0000

    Test

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
-import {View} from 'react-native';
+import React, { Component, PureComponent } from 'react';
+import {View, AsyncStorage} from 'react-native';
 import{addNavigationHelpers} from 'react-navigation';
 import Root from './src/navigations';
 import {connect} from 'react-redux';
-
-class App extends Component {
+import {
+getUser
+} from './src/actions';
+import Modal from './src/screen/popup/Modal'
+class App extends PureComponent {
+  constructor(){
+    super();
+    this.state={
+      loadding: true 
+    }
+  }
+  componentWillMount(){
+    this.props.getUser();
+    this.getAsyncLocal();
+  }
+  async getAsyncLocal(){
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      await AsyncStorage.multiGet(keys, (err, storages)=>{
+        storages.map((result, i, storage) =>{
+          console.log(result);
+          let key = storages[i][0];
+          let value = storages[i][1];
+        })
+      })
+    } catch (error) {
+      
+    }
+  }
   render() {
     return (
       <View style={{flex:1}}>
@@ -14,7 +41,9 @@ class App extends Component {
   }
 }
 const mapStateProps = (state)=>{
-  return{nav: state.nav}
+  return{
+    nav: state.nav
+  }
 }
 
-export default connect(mapStateProps)(App);
+export default connect(mapStateProps, {getUser})(App);
